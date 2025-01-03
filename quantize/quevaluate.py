77039187dcaf83466a3ce08de40afaab1eec37da
zjg_lz <3557727504@qq.com>
2025-01-03 15:58:16 +0800

    [quantize] evaluate & recover

---
+++ quantize/quevaluate.py
@@ -0,0 +1,83 @@
+import torch
+import os
+import sys
+sys.path.append('/home/lz/On-the-Fly_MoE_Inference')
+os.environ["CUDA_VISIBLE_DEVICES"] = "1,2,3"
+from transformers import AutoTokenizer, MixtralForCausalLM
+# from modeling_mixtral import MixtralForCausalLM
+from convert_model import convert_mixtral_model
+import json 
+
+with open('../path.json', 'r') as f:
+    path = json.load(f)
+    model_name = path['mixtral']
+
+with open('./device_map.json', 'r') as f:
+    device_map = json.load(f)
+
+llm = MixtralForCausalLM.from_pretrained(
+    model_name,
+    # device_map='auto',
+    device_map=device_map,
+    use_cache=True,
+    torch_dtype=torch.float16,
+    # attn_implementation="flash_attention_2"
+) 
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.pad_token_id = tokenizer.eos_token_id
+# convert_mixtral_model(llm, start_num=-1, end_num=32, gamma=0.2,)
+# for name, param in llm.named_parameters():
+#     print(name)
+
+#Quantize
+from hqq.core.quantize import *
+q4_config    = BaseQuantizeConfig(nbits=8, group_size=64, offload_meta=True) 
+q3_config    = BaseQuantizeConfig(nbits=2, group_size=32)
+
+quant_config = {
+  'block_sparse_moe.experts.w3'  :q3_config,
+}
+from hqq.models.hf.base import AutoHQQHFModel
+AutoHQQHFModel.quantize_model(llm, quant_config=quant_config, compute_dtype=torch.float16, device=device_map)
+
+# Test Model
+import os
+import json
+import torch
+from transformers import AutoTokenizer, MixtralForCausalLM
+from convert_model import convert_llama_model, convert_mixtral_model
+# export HF_ENDPOINT="https://hf-mirror.com"
+os.environ["HF_ENDPOINT"]="https://hf-mirror.com"
+
+import lm_eval
+from lm_eval.models.huggingface import HFLM
+from lm_eval import evaluator
+
+def evaluate(task_name_list, model, tokenizer, num_fewshot, device):
+    hflm = HFLM(pretrained=model, tokenizer=tokenizer)
+    results = evaluator.simple_evaluate(
+    model=hflm,
+    tasks=task_name_list,
+    num_fewshot=num_fewshot)
+    print(results['results'])
+
+task_name_list=['winogrande','sciq','openbookqa','arc_challenge','arc_easy']
+num_fewshot = 0
+with open('../path.json', 'r') as file:
+    paths = json.load(file)
+    model_path = paths.get('mixtral', '')
+
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "left"
+
+evaluate(task_name_list, llm, tokenizer, num_fewshot, 'cuda')
+
+# up: 2bit
+# {'arc_easy': {'acc,none': 0.8236531986531986, 'acc_stderr,none': 0.007820313817947478, 'acc_norm,none': 0.8202861952861953, 'acc_norm_stderr,none': 0.007878465068489398, 'alias': 'arc_easy'}, 
+# 'arc_challenge': {'acc,none': 0.5358361774744027, 'acc_stderr,none': 0.014573813664735622, 'acc_norm,none': 0.5802047781569966, 'acc_norm_stderr,none': 0.014422181226303012, 'alias': 'arc_challenge'}, 
+# 'openbookqa': {'acc,none': 0.358, 'acc_stderr,none': 0.021461434862859133, 'acc_norm,none': 0.464, 'acc_norm_stderr,none': 0.022324981738385333, 'alias': 'openbookqa'}, 
+# 'sciq': {'acc,none': 0.964, 'acc_stderr,none': 0.00589395781616553, 'acc_norm,none': 0.957, 'acc_norm_stderr,none': 0.006418114379799739, 'alias': 'sciq'}, 
+# 'winogrande': {'acc,none': 0.7426992896606156, 'acc_stderr,none': 0.012285989618865697, 'alias': 'winogrande'}}
---
+++ quantize/quevaluate2.py
@@ -0,0 +1,249 @@
+import torch
+import os
+import sys
+os.environ["CUDA_VISIBLE_DEVICES"] = "4,5,6"
+from transformers import AutoTokenizer, BitsAndBytesConfig, MixtralForCausalLM
+import json
+
+with open('../path.json', 'r') as f:
+    path = json.load(f)
+    model_name = path['mixtral']
+
+with open('./device_map.json', 'r') as f:
+    device_map = json.load(f)
+
+llm = MixtralForCausalLM.from_pretrained(
+    model_name,
+    device_map=device_map,
+    use_cache=True,
+    torch_dtype=torch.float16,
+) 
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.pad_token_id = tokenizer.eos_token_id
+
+# %%
+from datasets import load_dataset
+def preprocess_data(batch):
+    # 使用 tokenizer 将文本数据转换为模型输入
+    inputs = tokenizer(batch['text'], padding="max_length", truncation=True, max_length=512, return_tensors="pt")
+    inputs["labels"] = inputs.input_ids.clone()
+    return inputs
+
+# 定义一个函数来选择特征并丢弃不需要的
+def select_features(example):
+    return {
+        'input_ids': example['input_ids'],
+        'attention_mask': example['attention_mask'],
+        'labels': example['labels']
+    }
+
+tokenizer.pad_token = tokenizer.eos_token
+# # 加载 C4 数据集的验证集
+with open('../path.json', 'r') as file:
+    paths = json.load(file)
+    c4_path = paths.get('c4', '')
+c4 = load_dataset(c4_path)
+# 对数据集进行预处理
+c4_dataset = c4.map(preprocess_data, batched=True)
+# c4_dataset = c4_dataset.map(select_features, batched=True)
+c4_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'labels'])
+# c4_dataset
+top_four_thousand_data = c4_dataset['validation'].select(range(100))
+
+import numpy as np
+
+def set_seed(seed):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+set_seed(42)
+
+# 定义数据加载器
+batch_size = 4
+# dataloader = DataLoader(c4_dataset['validation'], batch_size=batch_size)
+dataloader = DataLoader(top_four_thousand_data, batch_size=batch_size)
+
+# 计算评估损失
+total_loss = 0.0
+num_batches = 0
+
+for batch in tqdm(dataloader):
+    input_ids = batch['input_ids'].to(llm.device)
+    attention_mask = batch['attention_mask'].to(llm.device)
+    labels = batch['labels'].to(llm.device)
+    
+    # 禁用梯度计算
+    with torch.no_grad():
+        outputs = llm(input_ids, attention_mask=attention_mask, labels=labels)
+        loss = outputs.loss
+        total_loss += loss.item()
+        num_batches += 1
+        if num_batches % 100 == 0:
+            print(f"[{num_batches}], Eval Loss: {total_loss / (num_batches)}")
+
+# 计算平均损失
+eval_loss = total_loss / num_batches
+print(f"Eval Loss: {eval_loss}")
+
+# %%
+import torch
+import os
+
+llm_base = MixtralForCausalLM.from_pretrained(
+    model_name,
+    device_map='cpu',
+    use_cache=True,
+    torch_dtype=torch.float16,
+    # attn_implementation="flash_attention_2"
+) 
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.pad_token_id = tokenizer.eos_token_id
+
+# %%
+def profle_svdllm(name, model, calib_loader, dev):
+    # model.to(dev)
+    if "llama" in name or "mixtral" in name or "vicuna" in name:
+        layers = model.model.layers
+    print("Start obtaining the whitening matrix...")
+    def hook(module, input, output):
+        inp = input[0].detach().float()
+        if inp.dim() == 2:   # for opt
+            inp = inp.unsqueeze(0)
+        adds = torch.matmul(inp.transpose(1,2), inp)
+        adds_sum = torch.sum(adds, dim=0)
+        module.raw_scaling_diag_matrix += adds_sum
+        del inp, adds, adds_sum
+        torch.cuda.empty_cache()
+    for name, module in model.named_modules():
+        if "w3" in name:
+            # print(name)
+            module.raw_scaling_diag_matrix = 0
+            module.register_forward_hook(hook)
+            
+    for batch in tqdm(calib_loader):
+        inputs = batch['input_ids'].to(llm.device)
+        model(inputs)
+    for name, module in model.named_modules():
+        if "w3" in name:
+            module._forward_hooks.clear()
+            # print(module.raw_scaling_diag_matrix)
+    torch.cuda.empty_cache()
+
+    profiling_mat = {}
+    print("Start Cholesky Decomposition...")
+    
+    layer_profile = {}
+    for name, module in model.named_modules():
+        if "w3" in name:
+            covariance = module.raw_scaling_diag_matrix.double().to(dev)
+            if not torch.allclose(covariance, covariance.t(), atol=1e-6):
+                raise ValueError("Covariance matrix is not symmetric.")
+                    # Perform eigen decomposition
+            Lambda, Q = torch.linalg.eigh(covariance, UPLO='U')
+            if torch.isnan(Lambda).any() or torch.isinf(Lambda).any():
+                raise ValueError("Lambda contains NaN or Inf values.")
+
+            # 检查 Lambda 是否包含负值
+            if (Lambda < 0).any():
+                print("Lambda contains negative values. Clamping to zero.")
+                eigenvalues = torch.linalg.eigvalsh(covariance)
+                covariance += (- eigenvalues[0] + 2e-6) * torch.eye(covariance.shape[0]).cuda()
+                Lambda, Q = torch.linalg.eigh(covariance, UPLO='U')
+                print(f"Lambda min: {Lambda.min().item()}, Lambda max: {Lambda.max().item()}")
+            # 现在进行平方根操作
+            Lambda_diag = torch.diag(torch.sqrt(Lambda))
+            # Sort eigenvalues and eigenvectors in descending order
+            indices = torch.argsort(Lambda, descending=True)
+            Lambda = Lambda[indices]
+            Q = Q[:, indices]
+
+            # Compute Q_prime = Q * sqrt(Lambda)
+            Lambda_diag = torch.diag(torch.sqrt(Lambda))
+            Q_prime = torch.matmul(Q, Lambda_diag)
+            layer_profile[name] = Q_prime.cpu()
+            profiling_mat[name] = layer_profile
+    return profiling_mat
+profiling_mat=profle_svdllm("mixtral", llm, dataloader, "cuda")
+
+# %% [markdown]
+# ### 量化
+
+# %%
+#Quantize
+from hqq.core.quantize import *
+q4_config    = BaseQuantizeConfig(nbits=8, group_size=64) 
+q3_config    = BaseQuantizeConfig(nbits=2, group_size=64)
+
+quant_config = {
+  'block_sparse_moe.experts.w3'  :q3_config,
+}
+from hqq.models.hf.base import AutoHQQHFModel
+AutoHQQHFModel.quantize_model(llm, quant_config=quant_config, compute_dtype=torch.float16, device=device_map)
+
+# %%
+class CompensatedModel(torch.nn.Module):
+    def __init__(self, model, B_prime, A):
+        super(CompensatedModel, self).__init__()
+        self.model = model
+        self.B_prime = torch.nn.Parameter(torch.tensor(B_prime).float())
+        self.A = torch.nn.Parameter(torch.tensor(A).float())
+        print(self.A.shape,self.B_prime.shape)
+    def forward(self, input_ids):
+        outputs = self.model(input_ids)
+        # 假设在特定层添加残差连接，根据实际模型结构进行修改
+        # print(self.B_prime.shape,self.A.shape,input_ids.shape)
+        residual = input_ids @ (self.B_prime @ self.A).T
+        outputs += residual
+    
+        return outputs
+    
+for i in range(32):
+    for j in range(8):
+        llmdevice = llm.model.layers[i].block_sparse_moe.experts[j].w3.device
+        Delta_W = llm_base.model.layers[i].block_sparse_moe.experts[j].w3.weight.to(llmdevice) - llm.model.layers[i].block_sparse_moe.experts[j].w3.dequantize()
+        Q_prime = profiling_mat[f"model.layers.{i}.block_sparse_moe.experts.{j}.w3"][f"model.layers.{i}.block_sparse_moe.experts.{j}.w3"].cuda().float()
+        Delta_W_prime =  Delta_W.to(torch.float16).to(llmdevice) @ Q_prime.to(torch.float16).to(llmdevice)
+        llm_base.model.layers[i].block_sparse_moe.experts[j].w3.cpu()
+        # 步骤5: 进行SVD分解并取前r个奇异值
+        rank = 1024  # 设置 desired rank
+        U_prime, Sigma_prime, V_prime = torch.linalg.svd(Delta_W_prime, full_matrices=False)
+        U_prime = U_prime[:, :rank]
+        Sigma_prime = Sigma_prime[:rank]
+        V_prime = V_prime[:rank, :]
+
+        B_prime = U_prime @ torch.diag(Sigma_prime)
+        A_prime = V_prime
+
+        # 步骤6: 投影回原空间
+        A = A_prime.to(llmdevice) @ torch.linalg.inv(Q_prime).to(llmdevice)
+        llm.model.layers[i].block_sparse_moe.experts[j].w3 = CompensatedModel(llm.model.layers[i].block_sparse_moe.experts[j].w3, B_prime, A).to(llmdevice)
+    # compensated_model = CompensatedModel(student.base, B_prime, A).to("cuda")
+
+import lm_eval
+from lm_eval.models.huggingface import HFLM
+from lm_eval import evaluator
+del dataloader
+
+# %%
+def evaluate(task_name_list, model, tokenizer, num_fewshot, device):
+    hflm = HFLM(pretrained=llm, tokenizer=tokenizer)
+    results = evaluator.simple_evaluate(
+    model=hflm,
+    tasks=task_name_list,
+    num_fewshot=num_fewshot)
+    print(results['results'])
+
+
+
+# triviaqa
+task_list=['winogrande','sciq','openbookqa','arc_challenge','arc_easy']
+evaluate(task_list, llm, tokenizer, 0, "cuda")
+
+
+
